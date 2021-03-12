a257ee518a785aa503f13abbafce0ab343adbce7
Renan Kanu <renankanu@gmail.com>
2021-03-12 02:51:32 -0300

    Init modular in project

--- lib/app/app_bloc.dart
+++ lib/app/app_bloc.dart
@@ -1,7 +1,6 @@
 import 'package:flutter_modular/flutter_modular.dart';
 
-class AppBloc extends Disposable {
-  //dispose will be called automatically by closing its streams
+class AppBloc extends BlocBase {
   @override
   void dispose() {}
 }
